65aaa36f4211eb7a816c5d5b0ade6f5f1cebb88b
themightyoarfish <rdiederichse@uos.de>
2017-09-20 11:37:59 +0200

    Do not change case of titles

--- unsrtwithlinks.py
+++ unsrtwithlinks.py
@@ -1,10 +1,21 @@
 from pybtex.style.formatting.unsrt import Style as UnsrtStyle
+from pybtex.style.template import field, sentence
 from pybtex.style import FormattedEntry
 from pybtex.richtext import Text, String, HRef, Tag
 
 
 class Style(UnsrtStyle):
     """Style for appending links to html output."""
+
+    def format_title(self, e, which_field, as_sentence=True):
+        formatted_title = field(
+            which_field
+        )
+
+        if as_sentence:
+            return sentence(capfirst=False) [ formatted_title ]
+        else:
+            return formatted_title
 
     def format_entries(self, entries):
         """Override for appending links before yielding FormattedEntries.
@@ -19,14 +30,18 @@ class Style(UnsrtStyle):
                     person.text = self.format_name(person,
                                                    self.abbreviate_names)
 
+            # if 'title' in entry.fields and 'Geodatenvisualisierung' in
+            # entry.fields['title']: import ipdb; ipdb.set_trace()
             f = getattr(self, "format_" + entry.type)
             text = f(entry)
             text = text + Text(String(' ['),
-                               HRef(entry.fields['publipy_biburl'], Tag('tt', 'bib')),
+                               HRef(entry.fields['publipy_biburl'],
+                                    Tag('tt', 'bib')),
                                String('] ')
                                )
             text = text + Text(String(' ['),
-                               HRef(entry.fields['publipy_pdfurl'], Tag('tt', 'pdf')),
+                               HRef(entry.fields['publipy_pdfurl'],
+                                    Tag('tt', 'pdf')),
                                String(']')
                                )
             yield FormattedEntry(entry.key, text, label)
