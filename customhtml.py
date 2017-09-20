a7e61ccfba987e986c1e6a58f3167cc576504809
themightyoarfish <rdiederichse@uos.de>
2017-09-20 20:36:12 +0200

    Add CSS styling

--- customhtml.py
+++ customhtml.py
@@ -1,4 +1,46 @@
+import pybtex
 from pybtex.backends.html import Backend as HTMLBackend
+
+PROLOGUE = '''<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+<head><meta name="generator" content="Pybtex">
+<meta http-equiv="Content-Type" content="text/html; charset={encoding}">
+<title>Bibliography</title>
+<style>
+{stylesheet}
+</style>
+</head>
+<body>
+<dl>
+'''
+
+STYLESHEET = r'''dl {
+    width: 100%;
+    overflow: hidden;
+    padding: 0;
+    margin: 0
+}
+dt {
+    float: left;
+    width: 5%;
+    padding: 0;
+    margin: 0;
+    text-align: center;
+}
+dt::before {
+    content: "["
+}
+dt::after {
+    content: "]"
+}
+dd {
+    float: left;
+    width: 95%;
+    padding: 0;
+    margin: 0;
+    margin-bottom: 1em
+}'''
+
 
 class Backend(HTMLBackend):
 
@@ -6,3 +48,7 @@ class Backend(HTMLBackend):
         # TODO: If CSS fails, do HTML stuff here
         self.output(u'<dt>%s</dt>\n' % label)
         self.output(u'<dd>%s</dd>\n' % text)
+
+    def write_prologue(self):
+        encoding = self.encoding or pybtex.io.get_default_encoding()
+        self.output(PROLOGUE.format(encoding=encoding, stylesheet=STYLESHEET))
