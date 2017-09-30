a2434b342bf64990d4f15353588dd16d7ae41508
themightyoarfish <rdiederichse@uos.de>
2017-09-30 12:31:35 +0200

    Abort script when compilation fails

--- tex/compile.sh
+++ tex/compile.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
+function exit_on_fail {
+    "$@"
+    local status=$?
+    if [ $status -ne 0 ]; then
+        echo "error with $1" >&2
+        exit $status
+    fi
+}
 echo "Compiling (1/3)..."
-pdflatex *.tex > /dev/null;
+exit_on_fail pdflatex -interaction nonstopmode *.tex > /dev/null;
 echo "Running bibtex (1/2)..."
 for f in *.aux; do bibtex "$f" > /dev/null; done
 echo "Running bibtex (2/2)..."
 for f in *.aux; do bibtex "$f" > /dev/null; done
 echo "Compiling (2/3)..."
-pdflatex *.tex > /dev/null;
+exit_on_fail pdflatex -interaction nonstopmode *.tex > /dev/null;
 echo "Compiling (3/3)..."
-pdflatex *.tex > /dev/null;
+exit_on_fail pdflatex -interaction nonstopmode *.tex > /dev/null;
 echo "Your document is now ready."
