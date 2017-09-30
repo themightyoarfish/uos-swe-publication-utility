c39cacdce41ad6b80c8c97c1dfb631b893e8aa63
themightyoarfish <rdiederichse@uos.de>
2017-09-30 12:31:09 +0200

    Update plugin locations

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(name='Pybtex HTML rendering extensions',
       author='Rasmus Diederichsen',
       py_modules=['unsrtwithlinks'],
       entry_points={
-          'pybtex.style.formatting': ['unsrtwithlinks = unsrtwithlinks:Style',
+          'pybtex.style.formatting': ['unsrtwithlinks = formatting.unsrtwithlinks:Style',
                                       'gerunsrtwithlinks =\
-                                      gerunsrtwithlinks:Style'
+                                      formatting.gerunsrtwithlinks:Style'
                                       ],
-          'pybtex.backends': 'customhtml = customhtml:Backend',
+          'pybtex.backends': 'customhtml = backends.customhtml:Backend',
           'pybtex.style.labels': 'numberwithoffset = labels.numberwithoffset:LabelStyle',
           'pybtex.style.sorting': 'custom_author_year_title = sorting.custom_author_year_title:SortingStyle',
           'pybtex.database.output': 'custombibtex = output.custombibtex:Writer',
