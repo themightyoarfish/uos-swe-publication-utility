9ed5ec580e24d94f348632dc9b00bfd5aaf0b230
themightyoarfish <rdiederichse@uos.de>
2017-11-18 12:46:42 +0100

    Add year-author sort + remove useless code

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ Install pybtex style plugin
 
 setup(name='Pybtex HTML rendering extensions',
       author='Rasmus Diederichsen',
-      py_modules=['unsrtwithlinks'],
       entry_points={
-          'pybtex.style.formatting': ['unsrtwithlinks = formatting.unsrtwithlinks:Style',
-                                      'gerunsrtwithlinks = formatting.gerunsrtwithlinks:Style'
-                                      ],
+          'pybtex.style.formatting': ['gerunsrtwithlinks = formatting.gerunsrtwithlinks:Style',
+                                      'unsrtwithlinks = formatting.unsrtwithlinks:Style'],
           'pybtex.backends': 'customhtml = backends.customhtml:Backend',
           'pybtex.style.labels': 'numberwithoffset = labels.numberwithoffset:LabelStyle',
-          'pybtex.style.sorting': 'custom_author_year_title = sorting.custom_author_year_title:SortingStyle',
+          'pybtex.style.sorting': ['custom_author_year_title = sorting.custom_author_year_title:SortingStyle',
+                                   'year_author = sorting.year_author:SortingStyle'
+                                   ],
           'pybtex.database.output': 'custombibtex = output.custombibtex:Writer',
       },
       )
