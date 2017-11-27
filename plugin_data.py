c9ed2ac102d25f740ef4486949abe622d0829624
themightyoarfish <rdiederichse@uos.de>
2017-11-27 12:29:14 +0100

    Add more documentation

--- plugin_data.py
+++ plugin_data.py
@@ -1,5 +1,25 @@
+'''
+.. module:: plugin_data
+
+Global data that must be passed to plugins. This is a crutch for circumventing
+the fact that Pybtex (or setuptools plugins) do not allow passing constructor
+arguments to the plugins.
+
+.. py:data:: plugin_data
+
+    A global :py:class:`dict` with the following fields
+
+        * ``label_start``, ``int``: Start value for the labels in a section.
+          This value can be updated by the labeling plugin so that numbers are
+          consecutive over sections.
+        * ``mapping_<key>``, :py:class:`OrderedDict`: Dictionaries for mapping
+          section names (which by default are the different values for the key
+          to group on) to different strings. The order of the names sets the
+          order in the output.
+'''
+
 from collections import OrderedDict
-# dirty global fields, but no way to pass args to LabelStyle or other plugin :|
+
 plugin_data = {
     'label_start': 10,
     'write_html_wrapper': True,
